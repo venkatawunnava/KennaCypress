9b26864f9fcb471efbc2ca9e2d868be40bc4ce28
venkata <venkata.wunnava@kennasecurity.com>
2021-01-27 18:42:11 +0530

    removed

--- cypress/integration/AddConnector/AddConnector.js
+++ cypress/integration/AddConnector/AddConnector.js
@@ -1,43 +1,43 @@
-import Connectors from '../../support/pages/Connectors'
-import HomePage from '../../support/pages/HomePage'
-import addConnectorData from '../../fixtures/addConnectorData.json'
-
-const connectors = new Connectors();
-const homePage = new HomePage();
-const connectorData = addConnectorData;
-
-describe.skip('Add Connectors and Run', () => {
-  beforeEach(function () {
-    cy.login();
-  });
-
-  connectorData.add_connector.forEach((addConnector) => {
-    it.skip('adding connector '+addConnector.connector, function () {
-      homePage.connectorTab().click();
-      connectors.connAdd().click();
-      console.log(addConnector.connector);
-      cy.log(addConnector.connector);
-      cy.get(addConnector.connector).click();
-      connectors.connSaveXML().click();
-    })
-  })
-
-
-  connectorData.uploadFile.forEach((connectorName) => {
-    it('uploads the file for '+connectorName.connectorName+' and runs the connector', function () {
-      homePage.connectorTab().click();
-
-      cy.get('.slider__item').each(($e1, index) => {
-
-        if ($e1.text().includes(connectorName.connectorName)) {
-          index += 1;
-          // eslint-disable-next-line cypress/no-force
-          connectors.btnUploadAndRun(index).click({force: true});
-          cy.get(connectors.uploadModalDiv(index)).attachFile('XMLFiles/' + connectorName.fileName, {subjectType: 'drag-n-drop'});
-          // eslint-disable-next-line cypress/no-unnecessary-waiting
-          cy.wait(10000)
-        }
-      })
-    })
-  })
-});
+// import Connectors from '../../support/pages/Connectors'
+// import HomePage from '../../support/pages/HomePage'
+// import addConnectorData from '../../fixtures/addConnectorData.json'
+//
+// const connectors = new Connectors();
+// const homePage = new HomePage();
+// const connectorData = addConnectorData;
+//
+// describe.skip('Add Connectors and Run', () => {
+//   beforeEach(function () {
+//     cy.login();
+//   });
+//
+//   connectorData.add_connector.forEach((addConnector) => {
+//     it.skip('adding connector '+addConnector.connector, function () {
+//       homePage.connectorTab().click();
+//       connectors.connAdd().click();
+//       console.log(addConnector.connector);
+//       cy.log(addConnector.connector);
+//       cy.get(addConnector.connector).click();
+//       connectors.connSaveXML().click();
+//     })
+//   })
+//
+//
+//   connectorData.uploadFile.forEach((connectorName) => {
+//     it('uploads the file for '+connectorName.connectorName+' and runs the connector', function () {
+//       homePage.connectorTab().click();
+//
+//       cy.get('.slider__item').each(($e1, index) => {
+//
+//         if ($e1.text().includes(connectorName.connectorName)) {
+//           index += 1;
+//           // eslint-disable-next-line cypress/no-force
+//           connectors.btnUploadAndRun(index).click({force: true});
+//           cy.get(connectors.uploadModalDiv(index)).attachFile('XMLFiles/' + connectorName.fileName, {subjectType: 'drag-n-drop'});
+//           // eslint-disable-next-line cypress/no-unnecessary-waiting
+//           cy.wait(10000)
+//         }
+//       })
+//     })
+//   })
+// });
